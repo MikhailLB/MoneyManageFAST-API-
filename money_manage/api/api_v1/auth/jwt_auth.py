a93628ef4cbf538c762177e58ff7817da29a7bd6
Mikhail <lobodamisha29@gmail.com>
2025-04-28 22:21:50 +0200

    Update jwt_auth.py

--- money_manage/api/api_v1/auth/jwt_auth.py
+++ money_manage/api/api_v1/auth/jwt_auth.py
@@ -36,7 +36,7 @@ async def get_current_user(
         if username is None:
             raise HTTPException(status_code=401, detail="Invalid token")
 
-        user = await get_user(username=username, session=session)  # нужно ждать!
+        user = await get_user(username=username, session=session)
         if user is None:
             raise HTTPException(status_code=401, detail="User not found")
         return user
